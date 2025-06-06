ca0e672673f645bc9526bb068f7847a8a3745582
Alyffantonio <146452808+Alyffantonio@users.noreply.github.com>
2025-06-06 08:57:04 -0300

    adicionei o docker compose e docker file

--- scripts_db/create_views.sql
+++ scripts_db/create_views.sql
@@ -1,19 +1,17 @@
-CREATE OR REPLACE VIEW fastest_lap_per_grand_prix AS
+CREATE VIEW fastest_lap_per_grand_prix AS
 SELECT
-    r.date AS "data da corrida",
-    r.name AS "nome do grand prix",
-    res.fastest_lap_time AS "tempo da volta mais rápida",
-    d.full_name AS "nome completo do corredor",
-    c.name AS "nome do construtor"
+    r.date AS race_date,
+    r.name AS grand_prix_name,
+    res.fastest_lap_time AS fastest_lap_time,
+    d.full_name AS driver_fullname,
+    c.name AS constructor_name
 FROM
     results res
-JOIN
-    races r ON res.race_id = r.race_id
-JOIN
-    drivers d ON res.driver_id = d.driver_id
-JOIN
-    constructors c ON res.constructor_id = c.constructor_id
+JOIN races r ON res.race_id = r.race_id
+JOIN drivers d ON res.driver_id = d.driver_id
+JOIN constructors c ON res.constructor_id = c.constructor_id
 WHERE
+    res.fastest_lap_time IS NOT NULL AND
     (res.race_id, res.fastest_lap_time) IN (
         SELECT
             sub.race_id,
@@ -25,26 +23,24 @@ WHERE
         GROUP BY
             sub.race_id
     )
-ORDER BY res.fastest_lap_time ASC;
-
-
-
-CREATE OR REPLACE VIEW racer_performance AS
-SELECT
-    y.year AS ano,
-    f.full_name AS "nome completo do corredor",
-    n.name AS "nome do construtor",
-    COUNT(*) FILTER (WHERE res.position_order = 1) AS "total de vitórias",
-    SUM(res.points) AS "total de pontos"
-FROM
-    results res
-JOIN
-    races y ON res.race_id = y.race_id
-JOIN
-    drivers f ON res.driver_id = f.driver_id
-JOIN
-    constructors n ON res.constructor_id = n.constructor_id
-GROUP BY
-    y.year, f.full_name, n.name
 ORDER BY
-    y.year, "total de vitórias" DESC;
+    r.date DESC;
+
+
+
+CREATE OR REPLACE VIEW resultado_corredores_por_ano AS
+SELECT
+    ra.year AS ano,
+    dr.full_name AS nome_completo_corredor,
+    co.name AS nome_construtor,
+    COUNT(CASE WHEN re.position_order = 1 THEN 1 END) AS qtd_vitorias,
+    SUM(re.points) AS qtd_pontos
+FROM
+    results re
+JOIN races ra ON re.race_id = ra.race_id
+JOIN drivers dr ON re.driver_id = dr.driver_id
+JOIN constructors co ON re.constructor_id = co.constructor_id
+GROUP BY
+    ra.year, dr.full_name, co.name
+ORDER BY
+    ra.year, dr.full_name;
